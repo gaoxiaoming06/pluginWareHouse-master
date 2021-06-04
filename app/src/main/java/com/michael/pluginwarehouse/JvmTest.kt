e5d582c7058c2670dd0462193ab2b7691fdae518
gaoxm <gaoxiaoming@erwan365.com>
2021-06-04 14:15:17 +0800

    测试lambda

--- app/src/main/java/com/michael/pluginwarehouse/JvmTest.kt
+++ app/src/main/java/com/michael/pluginwarehouse/JvmTest.kt
@@ -5,6 +5,10 @@ import android.view.View
 
 class JvmTest {
 
+    fun test111(): Unit {
+
+    }
+
     fun test(view: View, click: ClickMonitor): Boolean {
         click.onClick(view)
         return false
--- app/src/main/java/com/michael/pluginwarehouse/MainActivity.kt
+++ app/src/main/java/com/michael/pluginwarehouse/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.animation.Animation
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.Transition
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -28,9 +31,50 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             Log.d(TAG, "-------btnTestBackLamba------")
         }
 
-        findViewById<Button>(R.id.btnTestBlock).setOnClickListener {
-            Exception().printStackTrace()
-            Log.d(TAG, "-------btnTestBlock------")
+        val itemExamBinding = JvmTest()
+        var questionAnswer: QuestionAnswer = QuestionAnswer()
+        var b : Float = 5f
+        var view: View = View(this)
+        var c: Animation? = null
+        var questionTv: TextView = TextView(this)
+        var e: Button = Button(this)
+        var optionViewList  = listOf<Int>(1,1,5)
+        var map = mutableMapOf(4 to 8, 9 to 7)
+        var isMulti = false
+        findViewById<Button>(R.id.btnTestBlock).setOnClickListener { //反选
+            itemExamBinding.test111()
+            if (!isMulti) {
+                //单选逻辑处理 取消其他选择
+                if (optionViewList.size > 0) {
+                    for (j in optionViewList.indices) {
+                        if (j == 1 || !(optionViewList[1] as Boolean)) {
+                            continue
+                        }
+                        optionViewList[j]
+                    }
+                }
+            }
+            //选择逻辑
+            if (!isMulti) {
+                //单选 清空已选
+                map.clear()
+            }
+                map.put(5,9)
+                map.remove(4)
+
+            //给问题的tv设置tag标记 多选必须多选
+            questionTv.tag = !isMulti || map.size > 1
+            for (key in 0 until map.size) {
+                b
+                view.post({b.compareTo(8)})
+            }
+            if (map.isNotEmpty()) {
+                questionAnswer.answerOption = questionAnswer.answerOption!!.substring(
+                    0,
+                    questionAnswer.answerOption!!.length - 1
+                )
+            }
+            e.isEnabled = map.size == 8
         }
 
         findViewById<Button>(R.id.btnTestBlock2222).setOnClickListener(this)
@@ -41,7 +85,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
         })
     }
-
+    class QuestionAnswer {
+        var questionId: Long = 0
+        var answerOption: String? = null
+    }
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btnTestBlock2222 -> {
